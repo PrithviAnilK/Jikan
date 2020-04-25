67960a750412b6d22604f6ca59f44d3435752d8c
Prithvi Anil Kumar <thepirateking3d2y@gmail.com>
2020-04-25 14:51:02 +0530

    Background and base time added

--- jikanapp/src/Components/App.js
+++ jikanapp/src/Components/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Timer from './Timer'
 import Menu from './Menu'
-
+import '../Styles/App.css'
 
 class App extends React.Component {
     render() {
--- jikanapp/src/Components/Timer.js
+++ jikanapp/src/Components/Timer.js
@@ -4,22 +4,43 @@ import '../Styles/Timer.css';
 
 class Timer extends React.Component {
     state = {
-        time: 0
+        isOn : false,
+        time: {
+            minutes: 0,
+            seconds: "00"
+        }
     }
 
     startButton = () => {
-        console.log("yeye");
+        this.setState({
+            time: {
+                minutes: 25,
+                seconds: "00"
+            }
+        })
+    }
+    
+    stopButton = () => {
+    }
+    
+    resetButton = () => {
+        this.setState({
+            time: {
+                minutes: 0,
+                seconds: "00"
+            }
+        })
     }
 
     render() {
         return (
             <div className = "body">
                 <div className = "ui raised very padded text container segment">
-                    <h2 className = "ui header">{this.state.time}</h2>
+                    <h2 className = "ui header">{this.state.time.minutes + ":" + this.state.time.seconds}</h2>
                     <div className ="three ui buttons">
-                        <button className = "ui button green">Start</button>
-                        <button className = "ui button red">Stop</button>
-                        <button className = "ui button black">Reset</button>
+                        <button className = "ui button green" onClick = {this.startButton}>Start</button>
+                        <button className = "ui button red" onClick = {this.stopButton}>Stop</button>
+                        <button className = "ui button black" onClick = {this.resetButton}>Reset</button>
                     </div>
                 </div>
             </div>
